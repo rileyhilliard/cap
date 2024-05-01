77a46de2941e45760b0c4bfa6f86907c75cc5eff
Riley Hilliard <rileyhilliard@users.noreply.github.com>
2024-05-01 10:09:56 -0500

    upgrade everything and install vite for the backed server

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Kill any dangling ports 
+lsof -ti:3000 | xargs kill && lsof -ti:4000 | xargs kill
+
 # Kill the existing tmux session named mySession, if it exists
 tmux kill-session -t mySession 2>/dev/null
 
 # Start a new tmux session in the background
 tmux new-session -d -s mySession 
 
+# Run the start command for package-b (backend) in the main pane
+tmux send-keys -t mySession:0 'yarn start:backend' C-m 
+
 # Split tmux window vertically
 tmux split-window -h 
 
-# Run the start command for package-a in the left pane (0)
-tmux send-keys -t mySession:0.0 'lerna run start --scope @estatemetrics/frontend' C-m 
-
-# Run the start command for package-b in the right pane (1)
-tmux send-keys -t mySession:0.1 'lerna run start --scope @estatemetrics/backend' C-m 
+# Run the start command for package-a (frontend) in the right pane (1)
+tmux send-keys -t mySession:0.1 'sleep 3 && yarn start:frontend' C-m 
 
 # Attach to the tmux session so you can see the output
 tmux attach -t mySession
