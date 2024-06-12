51b78f0323f75ebac6b05f9ac39548d0805afc15
Riley Hilliard <rileyhilliard@users.noreply.github.com>
2024-06-12 12:12:11 -0500

    log file command update

--- packages/backend/scripts/start_server.sh
+++ packages/backend/scripts/start_server.sh
@@ -4,4 +4,8 @@
 yarn pm2 start pm2.config.cjs --env production
 
 # Keep the container running
-tail -f "./logs/$(ls -t ./logs | head -n1)"
+if [ -f "/app/logs/server-0.log" ]; then
+    tail -f "/app/logs/server-0.log"
+else
+    tail -f "/dev/null"
+fi
